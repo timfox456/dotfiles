cf361a4ee2dced85ca8f1b6dafb170de483bcaef
Tim Fox <tfox@createksolutions.com>
2024-10-20 09:01:27 -0400

    Fixed tsserver to ts_ls

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -40,7 +40,7 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
       lspconfig.ruff_lsp.setup({
