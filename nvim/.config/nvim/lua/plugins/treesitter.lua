6cc0546bcb663940efc1e41774c7606f2f29768a
Tim Fox <tfox@createksolutions.com>
2026-08-28 16:23:21 -0400

    Migrate treesitter config to new main-branch API (configs module removed upstream)

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,23 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
   config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
+    require("nvim-treesitter").setup({
       auto_install = true,
-      ensure_installed = { 'lua', 'python', 'regex', 'bash', 'markdown', 'markdown_inline', 'sql', 'vimdoc', 'javascript', 'typescript', 'tsx', 'json', 'html', 'yaml' },
-      highlight = {enable = true},
-      indent = {enable = true},
     })
-  end
+    require("nvim-treesitter").install({
+      'lua', 'python', 'regex', 'bash', 'markdown', 'markdown_inline',
+      'sql', 'vimdoc', 'javascript', 'typescript', 'tsx', 'json', 'html', 'yaml',
+    })
+
+    -- highlight/indent modules are gone in the main-branch rewrite:
+    -- highlighting is native (vim.treesitter.start) since 0.11, indent via the plugin's indentexpr
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+        vim.bo[args.buf].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+      end,
+    })
+  end,
 }
