d325fbfe070e84df67476a64fee51c7ce65603bc
Tim Fox <tfox@createksolutions.com>
2026-08-30 08:27:44 -0400

    Add Codeium autocomplete as blink.cmp source

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -9,9 +9,23 @@ return {
       appearance = { nerd_font_variant = "mono" },
       completion = { documentation = { auto_show = true } },
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lsp", "path", "snippets", "buffer", "codeium" },
+        providers = {
+          codeium = { name = "codeium", module = "codeium.blink", async = true },
+        },
       },
       signature = { enabled = true },
+    },
+  },
+  {
+    "Exafunction/codeium.nvim",
+    event = "InsertEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ua", function() require("codeium").toggle() end, desc = "Toggle Codeium" },
+    },
+    opts = {
+      enable_chat = false,
     },
   },
   {
